b7513eefb778ef3b82b14818e395f254a5115c30
Giulia <giuggy.rinaldi@gmail.com>
2016-11-01 14:00:26 +0100

    secondo commit

--- PROGETTO_TESI/OpenCL/kernel.cl
+++ PROGETTO_TESI/OpenCL/kernel.cl
@@ -1,20 +1,17 @@
-__kernel void  (__global const char *hashes, __global int *retu, __global const char *s, size_t numLines) {
+__kernel void search (__global const char *hashes, __global int *retu, __global const char *s, long numLines) {
  
 	// confronto tra due stringhe
 	int i = get_global_id(0);
 
 	if (i >= numLines) return;
 	int k;	
-	int flag=0;
 	for (k = 0; k< 33; k++){
 	
-		if (hashes[i * 33 + k] == s[k]) retu[k] = 0;
+		if (hashes[i* 33 + k] == s[k]) retu[k] = 0;
 		else{
 			retu[k] = 1;
-			flag=1;
 		}
 	}
-	if(!flag) return;
 
 }
 
--- PROGETTO_TESI/OpenCL/sort.c
+++ PROGETTO_TESI/OpenCL/sort.c
@@ -52,64 +52,133 @@ int chiamaKernel(char** hashes, char* s, size_t numLines){
 		cl_uint ret_num_devices;
 		cl_uint ret_num_platforms;
 		cl_int ret = clGetPlatformIDs(1, &platform_id, &ret_num_platforms);
-		if (ret != CL_SUCCESS) exit(fprintf(stderr, "error in clGetPlatformIDs\n"));
+		if (ret != CL_SUCCESS) {
+			printf("error in clGetPlatformIDs\n");
+			exit(1);
+		}
+		
 
 		ret = clGetDeviceIDs(platform_id, CL_DEVICE_TYPE_ALL, 1, 
 				&device_id, &ret_num_devices);
-		if (ret != CL_SUCCESS) exit(fprintf(stderr, "error in clGetDeviceIDs\n"));
- 
+		if (ret != CL_SUCCESS) {
+			ret = clGetDeviceIDs(platform_id, CL_DEVICE_TYPE_CPU, 1, 
+					&device_id, NULL);
+			if (ret != CL_SUCCESS) {
+				printf("error in clGetDeviceIDs\n");
+				exit(1);
+			}
+		}
 		// Create an OpenCL context
 		cl_context context = clCreateContext( NULL, 1, &device_id, NULL, NULL, &ret);
- 
+		if (ret != CL_SUCCESS) {
+			printf("error in cl_context\n");
+			exit(1);
+		}
 		// Create a command queue
 		cl_command_queue command_queue = clCreateCommandQueueWithProperties(context, device_id, 0, &ret);
- 
+		if (ret != CL_SUCCESS) {
+			printf("error in cl_command_queue\n");
+			exit(1);
+		}
 		
 		
 		// Create memory buffers on the device for each vector 
-		cl_mem ret_mem_obj = clCreateBuffer(context, CL_MEM_WRITE_ONLY | CL_MEM_READ_ONLY, 
+		cl_mem ret_mem_obj = clCreateBuffer(context, CL_MEM_WRITE_ONLY, 
 				LIST_SIZE * sizeof(int), NULL, NULL); 
+		if (!ret_mem_obj) {
+			printf("error in cl_mem ret\n");
+			exit(1);
+		}		
+				
+				
 		cl_mem hash_mem_obj = clCreateBuffer(context, CL_MEM_READ_ONLY,
-				LIST_SIZE* numLines * sizeof(char), C, NULL);
-		cl_mem s_mem_obj = clCreateBuffer(context, CL_MEM_READ_ONLY, 
-				LIST_SIZE * sizeof(int), s, NULL);
+				LIST_SIZE* numLines * sizeof(char), NULL, NULL);
+		if (!hash_mem_obj) {
+			printf("error in cl_mem hash\n");
+			exit(1);
+		}		
 		
+		cl_mem s_mem_obj = clCreateBuffer(context, CL_MEM_READ_ONLY, 
+				LIST_SIZE * sizeof(int), NULL, NULL);
+		if (!s_mem_obj) {
+			printf("error in cl_mem s\n");
+			exit(1);
+		}
 		
 		
 		// Copy the lists A and B to their respective memory buffers
 		ret = clEnqueueWriteBuffer(command_queue, hash_mem_obj, CL_TRUE, 0,
 				LIST_SIZE* numLines * sizeof(char), C, 0, NULL, NULL);
+		if (ret != CL_SUCCESS) {
+			printf("error in clEnqueueWriteBuffer\n");
+			exit(1);
+		}
 		ret = clEnqueueWriteBuffer(command_queue, s_mem_obj, CL_TRUE, 0, 
 				LIST_SIZE * sizeof(char), s, 0, NULL, NULL);
+		if (ret != CL_SUCCESS) {
+			printf("error in clEnqueueWriteBuffer\n");
+			exit(1);
+		}
  
 		// Create a program from the kernel source
 		cl_program program = clCreateProgramWithSource(context, 1, 
 				(const char **)&source_str, (const size_t *)&source_size, &ret);
- 
+		if (ret != CL_SUCCESS) {
+			printf("error in clCreateProgram\n");
+			exit(1);
+		}
 		// Build the program
 		ret = clBuildProgram(program, 1, &device_id, NULL, NULL, NULL);
- 
+		if (ret != CL_SUCCESS) {
+			
+			size_t len;
+			char buff[16384];
+
+			printf ("failed to build program executable\n");
+
+			// get OpenCL C compiler messages
+			clGetProgramBuildInfo(program, device_id, 
+								  CL_PROGRAM_BUILD_LOG, sizeof(buff), buff, &len);
+
+			// write OpenCL C compiler messages to stderr
+			printf("%s\n", buff);
+
+			// bail out
+			exit(1);
+		}
 		// Create the OpenCL kernel
 		cl_kernel kernel = clCreateKernel(program, "search", &ret);
  
 		// Set the arguments of the kernel
 		ret = clSetKernelArg(kernel, 0, sizeof(cl_mem), (void *)&hash_mem_obj);
-		ret = clSetKernelArg(kernel, 1, sizeof(cl_mem), (void *)&ret_mem_obj);
-		ret = clSetKernelArg(kernel, 2, sizeof(cl_mem), (void *)&s_mem_obj);
-		ret = clSetKernelArg(kernel, 3, sizeof(size_t), (void *)&numLines);
-		
+		ret |= clSetKernelArg(kernel, 1, sizeof(cl_mem), (void *)&ret_mem_obj);
+		ret |= clSetKernelArg(kernel, 2, sizeof(cl_mem), (void *)&s_mem_obj);
+		ret |= clSetKernelArg(kernel, 3, sizeof(long), (void *)&numLines);
+		if (ret != CL_SUCCESS) {
+			printf("error in clSetKernelArg\n");
+			exit(1);
+		}
  
 		// Execute the OpenCL kernel on the list
-		size_t global_item_size = numLines; // Process the entire lists
-		size_t local_item_size = 64; // Divide work items into groups of 64
+		//size_t global_item_size = numLines; // Process the entire lists
+		//size_t local_item_size = 64; // Divide work items into groups of 64
+		size_t global_item_size = (long) numLines; // Process the entire lists
+		size_t local_item_size = 8; // Divide work items into groups of 8                      MODIFICATO 64
+		global_item_size = (global_item_size + local_item_size - 1) / local_item_size * local_item_size;
 		ret = clEnqueueNDRangeKernel(command_queue, kernel, 1, NULL, 
 				&global_item_size, &local_item_size, 0, NULL, NULL);
-	 
+		if (ret != CL_SUCCESS) {
+			printf("error in clEnqueueNDRangeKernel\n");
+			exit(1);
+		}
 		// Read the memory buffer C on the device to the local variable C
 		
 		ret = clEnqueueReadBuffer(command_queue, ret_mem_obj, CL_TRUE, 0, 
 				LIST_SIZE * sizeof(int), retu, 0, NULL, NULL);
-		
+		if (ret != CL_SUCCESS) {
+			printf("error in clEnqueueReadBuffer\n");
+			exit(1);
+		}
 		
 		// Display the result to the screen
 		int flagP =0;
@@ -123,7 +192,9 @@ int chiamaKernel(char** hashes, char* s, size_t numLines){
 		
 		if(flagP == 1) flag=1;
 		else flag = 0;
-		
+		printf("%s ", s);
+		printf("%d ", flag);
+		printf("%s \n", hashes[0]);
 		
 		//liberazione risorse
 		
